bd69bb37b6a516278265f675891695db095884f6
Munira Begmuratova <mbegmuratova@gmail.com>
2019-07-05 09:12:04 +0500

    solved

--- src/index.js
+++ src/index.js
@@ -1,3 +1,32 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let leftBrackets = [];
+  let rightBrackets = [];
+  let similarBrackets = [];
+  for (let i = 0; i < bracketsConfig.length; i++) {
+    leftBrackets.push(bracketsConfig[i][0]);
+    rightBrackets.push(bracketsConfig[i][1]);
+    if (bracketsConfig[i][0] === bracketsConfig[i][1]) {
+      similarBrackets.push(bracketsConfig[i][0]);
+    }
+  }
+  let stackOfBrackets = [];
+  for (let i = 0; i < str.length; i++) {
+    if (leftBrackets.includes(str[i])) {
+      if (similarBrackets.includes(str[i]) && stackOfBrackets[stackOfBrackets.length - 1] === str[i]) {
+        stackOfBrackets.pop();
+      } else {
+        stackOfBrackets.push(str[i]);
+      }
+    } else if (rightBrackets.includes(str[i])) {
+      index = rightBrackets.indexOf(str[i]);
+      if (leftBrackets[index] === stackOfBrackets[stackOfBrackets.length - 1]) {
+        stackOfBrackets.pop();
+      } else {
+        return false;
+      }
+    } else {
+      return false;
+    }
+  }
+  return stackOfBrackets.length === 0 ? true : false;
 }
